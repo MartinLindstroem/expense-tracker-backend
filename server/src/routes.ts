02faccd25bbbee799e711262c162440aa837fb12
Martin Lindström <martinllindstroem@gmail.com>
2024-02-26 17:04:55 +0100

    update msg

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -3,7 +3,7 @@ import { Router, Request, Response } from "express";
 const router: Router = Router();
 
 router.get("/healthz", async (req: Request, res: Response) => {
-    res.status(200).json("Server is up");
+    res.status(200).json("Server is up!");
 });
 
 export default router;
