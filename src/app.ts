e4027af028c85984192644e733d37ca36336c15c
Martin <martinllindstroem@gmail.com>
2024-09-24 20:47:50 +0200

    add routers and middleware

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,15 @@
 import express, { Express } from "express";
-import dotenv from "dotenv";
+import "dotenv/config";
 import v1Router from "./v1/routes";
 import expenseRouter from "./v1/routes/expenseRoutes";
-
-dotenv.config();
+import categoryRouter from "./v1/routes/categoryRoutes";
+import errorMiddleware from "./middlewares/errorHandler";
 
 const app: Express = express();
+app.use(express.json());
 app.use("/api/v1", v1Router);
-app.use("/api/v1/expenses", expenseRouter)
+app.use("/api/v1/expenses", expenseRouter);
+app.use("/api/v1/categories", categoryRouter);
+app.use(errorMiddleware);
 
 export default app;
