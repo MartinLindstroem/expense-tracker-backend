862eb1e4daf1b45f7132347aafff52c017293247
Martin <martinllindstroem@gmail.com>
2024-09-24 20:48:31 +0200

    add controllers for expenses

--- src/controllers/expenseController.ts
+++ src/controllers/expenseController.ts
@@ -1,21 +1,90 @@
-import { Request, Response } from "express";
-// import { getAllExpensesFromDb } from "../database/expenses";
+import { Request, Response, NextFunction } from "express";
+import {
+  getAllUserExpenses,
+  getExpensesFromYear,
+  createExpense,
+  updateExpense,
+  deleteExpense,
+} from "../database/expenses";
 
-// const getAllExpenses = async (req: Request, res: Response) => {
-//     await getAllExpensesFromDb()
-//     res.status(200).json(`Get all expenses for user ${req.params.userId}`);
-// }
+export const getAllExpensesController = async (req: Request, res: Response, next: NextFunction) => {
+  try {
+    const result = await getAllUserExpenses(req.params.userId);
 
-const getExpensesForYear = (req: Request, res: Response) => {
-    res.status(200).json(`Get all expenses for ${req.params.year} for user ${req.params.userId}`);
-}
+    res.status(200).json(result.rows);
+  } catch (error) {
+    next(error);
+  }
+};
 
-const getExpensesForMonthAndYear = (req: Request, res: Response) => {
-    res.status(200).json(`Get all expenses for ${req.params.month} ${req.params.year} for user ${req.params.userId}`);
-}
+export const getExpensesFromYearController = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    const result = await getExpensesFromYear(req.params.userId, req.params.year);
 
-module.exports = {
-    // getAllExpenses,
-    getExpensesForMonthAndYear,
-    getExpensesForYear
-}
+    res.status(200).json(result.rows);
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const getExpensesForMonthAndYearController = (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    res
+      .status(200)
+      .json(
+        `Get all expenses for ${req.params.month} ${req.params.year} for user ${req.params.userId}`
+      );
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const createExpenseController = async (req: Request, res: Response, next: NextFunction) => {
+  try {
+    await createExpense(
+      req.body.userId,
+      req.body.name,
+      req.body.amount,
+      req.body.date,
+      req.body.category
+    );
+
+    res.status(201).json({ status: 201, message: "Expense created" });
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const updateExpenseController = async (req: Request, res: Response, next: NextFunction) => {
+  try {
+    await updateExpense(
+      req.body.id,
+      req.body.name,
+      req.body.amount,
+      req.body.date,
+      req.body.category
+    );
+
+    res.status(201).json({ status: 201, message: "Expense updated" });
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const deleteExpenseController = async (req: Request, res: Response, next: NextFunction) => {
+  try {
+    await deleteExpense(req.body.id);
+
+    res.status(200).json({ status: 200, message: "Expense deleted" });
+  } catch (error) {
+    next(error);
+  }
+};
