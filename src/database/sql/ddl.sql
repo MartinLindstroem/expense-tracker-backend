708947594964b62c971c23470ff5486caca5be62
Martin <martinllindstroem@gmail.com>
2024-10-30 20:51:17 +0100

    add test sql

--- src/database/sql/ddl.sql
+++ src/database/sql/ddl.sql
@@ -1,19 +1,33 @@
 CREATE DATABASE expense_tracker;
 
+CREATE TABLE IF NOT EXISTS users(
+    user_id SERIAL PRIMARY KEY,
+    email VARCHAR(100) UNIQUE NOT NULL,
+    username VARCHAR(20) UNIQUE NOT NULL,
+    password_hash VARCHAR(80) NOT NULL,
+    created DATE NOT NULL
+);
+
 CREATE TABLE IF NOT EXISTS categories(
-    id SERIAL PRIMARY KEY,
+    category_id SERIAL PRIMARY KEY,
     category_name VARCHAR(50) NOT NULL,
-    user_id VARCHAR(50)
+    user_id INTEGER,
+    CONSTRAINT user_id
+        FOREIGN KEY(user_id)
+        REFERENCES users(user_id)
 );
 
 CREATE TABLE IF NOT EXISTS expenses(
-    id SERIAL PRIMARY KEY,
-    user_id VARCHAR(255) NOT NULL,
+    expense_id SERIAL PRIMARY KEY,
+    user_id INTEGER NOT NULL,
     expense_name VARCHAR(50) NOT NULL,
     expense_amount INTEGER NOT NULL,
     expense_date DATE NOT NULL,
     expense_category INT NOT NULL,
     CONSTRAINT expense_category
       FOREIGN KEY(expense_category) 
-      REFERENCES categories(id)
+      REFERENCES categories(category_id),
+    CONSTRAINT user_id
+        FOREIGN KEY(user_id)
+        REFERENCES users(user_id)
 );
--- src/database/sql/dml.sql
+++ src/database/sql/dml.sql
@@ -8,13 +8,13 @@ INSERT INTO categories (category_name) VALUES
     ('Transport'),
     ('Travel');
 
-INSERT INTO expenses(user_id, expense_name, expense_amount, expense_date, expense_category) VALUES ('123', 'Sushi', '220', '2024-07-13', '4');
-INSERT INTO expenses(user_id, expense_name, expense_amount, expense_date, expense_category) VALUES ('123', 'Netflix', '179', '2024-07-25', '2');
-INSERT INTO expenses(user_id, expense_name, expense_amount, expense_date, expense_category) VALUES ('123', 'Jeans', '799', '2024-07-16', '3');
-INSERT INTO expenses(user_id, expense_name, expense_amount, expense_date, expense_category) VALUES ('123', 'Car', '90000', '2021-12-16', '1');
-INSERT INTO expenses(user_id, expense_name, expense_amount, expense_date, expense_category) VALUES ('999', 'The Last of Us 2', '549', '2024-09-16', '2');
-INSERT INTO expenses(user_id, expense_name, expense_amount, expense_date, expense_category) VALUES ('999', 'Tires', '5000', '2023-12-11', '1');
-INSERT INTO expenses(user_id, expense_name, expense_amount, expense_date, expense_category) VALUES ('999', 'Pension', '2000', '2024-09-26', '6');
+INSERT INTO expenses(user_id, expense_name, expense_amount, expense_date, expense_category) VALUES ('1', 'Sushi', '220', '2024-07-13', '4');
+INSERT INTO expenses(user_id, expense_name, expense_amount, expense_date, expense_category) VALUES ('1', 'Netflix', '179', '2024-07-25', '2');
+INSERT INTO expenses(user_id, expense_name, expense_amount, expense_date, expense_category) VALUES ('1', 'Jeans', '799', '2024-07-16', '3');
+INSERT INTO expenses(user_id, expense_name, expense_amount, expense_date, expense_category) VALUES ('1', 'Car', '90000', '2021-12-16', '1');
+INSERT INTO expenses(user_id, expense_name, expense_amount, expense_date, expense_category) VALUES ('30', 'The Last of Us 2', '549', '2024-09-16', '2');
+INSERT INTO expenses(user_id, expense_name, expense_amount, expense_date, expense_category) VALUES ('30', 'Tires', '5000', '2023-12-11', '1');
+INSERT INTO expenses(user_id, expense_name, expense_amount, expense_date, expense_category) VALUES ('30', 'Pension', '2000', '2024-09-26', '6');
 
 SELECT e.expense_name, e.expense_amount, c.category_name as category, e.expense_date
 FROM expenses e
