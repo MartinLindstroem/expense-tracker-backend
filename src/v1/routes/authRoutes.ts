5e44eb51484674f8d86c95632f6a3baece494d51
Martin Lindström <martinllindstroem@gmail.com>
2025-01-25 18:00:32 +0100

    add logout

--- src/v1/routes/authRoutes.ts
+++ src/v1/routes/authRoutes.ts
@@ -1,11 +1,17 @@
 import { Router, Request, Response } from "express";
-import { createUserController, loginController } from "../../controllers/authController";
+import {
+  createUserController,
+  loginController,
+  logoutController,
+} from "../../controllers/authController";
 
 const authRouter: Router = Router();
 
 authRouter.post("/signup", createUserController);
 
 authRouter.post("/login", loginController);
+
+authRouter.post("/logout", logoutController);
 
 // expenseRouter.get("/:userId/:month/:year", getExpensesForMonthAndYearController);
 
